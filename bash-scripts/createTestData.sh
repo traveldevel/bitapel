9e40cef6d545fcee3aa2a7aadb44580324a248a4
Travel Devel <traveldevel@hotmail.com>
2017-11-01 23:32:52 +0200

    Resource renamed to Thing in bna

--- bash-scripts/createTestData.sh
+++ bash-scripts/createTestData.sh
@@ -8,8 +8,8 @@ curl -X POST --header 'Content-Type: application/json' --header 'Accept: applica
 
 sleep 1
 
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"$class": "org.bitapel.model.Resource","id": "test","owner":"resource:org.bitapel.model.User#id:test","serial": "testserial","category": "test category","manufacturer": "test manufacturer","type": "test type","buyDate": "2017-10-31T14:40:46.015Z"}' 'http://localhost:3000/api/Resource' > /dev/null
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"$class": "org.bitapel.model.Thing","id": "test","owner":"resource:org.bitapel.model.User#id:test","serial": "testserial","category": "test category","manufacturer": "test manufacturer","type": "test type","buyDate": "2017-10-31T14:40:46.015Z"}' 'http://localhost:3000/api/Thing' > /dev/null
 
 sleep 1
 
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"$class": "org.bitapel.model.InfoEvent","resource": "resource:org.bitapel.model.Resource#id:test","owner": "resource:org.bitapel.model.User#id:test", "recordedBy": "test","totalWorkingUnits": 1,"totalWorkingUnitType": "h","infoDate": "2017-10-31T14:46:44.705Z","infoDetails": "test info"}' 'http://localhost:3000/api/InfoEvent' > /dev/null
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"$class": "org.bitapel.model.InfoEvent","resource": "resource:org.bitapel.model.Thing#id:test","owner": "resource:org.bitapel.model.User#id:test", "recordedBy": "test","totalWorkingUnits": 1,"totalWorkingUnitType": "h","infoDate": "2017-10-31T14:46:44.705Z","infoDetails": "test info"}' 'http://localhost:3000/api/InfoEvent' > /dev/null
